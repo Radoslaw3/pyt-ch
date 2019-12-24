ee8421de8501845290c6f773894347e5e9c898da
radoslawd3 <radoslaw.drabczyk@dxc.com>
2019-12-24 11:19:15 +0100

    start

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,7 +7,68 @@ public class main {
 }
 
 /*
+// passleader
+122.
+import java.unit.*;
+????
+public class Ref
+{
+public static void main(String[] args)
+{
+StringBuilder s1 = new StringBuilder("Hello Java!");
+String s2 = s1.toString();
+List<String> lst = new ArrayList<String>();
+lst.add(s2);
+System.out.println(s1.getClass());
+System.out.println(s2.getClass());
+System.out.println(lst.getClass());
+}
+}
 
+123.
+public class Case{
+public static void main(String[] args){
+String product = "Pen";
+product.toLowerCase();
+product.concat(" Box".toLowerCase());
+System.out.print(product.substring(4,6));
+}
+}
+
+124.
+public class Whizlabs{
+Psvm{
+int sum = 0;
+for (int x =0; x<=10; x++){
+sum += x;
+sout "Sum for 0 to " + x
+sout " = " + sum;
+}
+}
+}
+
+125.
+System.out.println(28+5<=4+29);
+System.out.println((28+5) <= (4+29));
+
+126.
+public class Equal{
+psvm
+String str1 = "Java";
+String[] str2 = {"J","a","v","a"};
+String str3 = "";
+for(String str : str2){
+str3 = str3+str;
+}
+boolean b1 = (str1 == str3);
+boolean b2 = (str1.equals(str3));
+System.out.print(b1+", "+b2);
+}
+
+127.
+public class Test{
+
+}
 
 
  */
