7bf3a781b0260c6790856ed0007deb1be6ef9a59
radoslawd3 <radoslaw.drabczyk@dxc.com>
2020-01-08 20:44:51 +0100

    po_128

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,7 @@
 public class main {
 
 
+
 }
 
 /*
@@ -82,4 +83,22 @@ System.out.print(b1+", "+b2);
         }
         //System.out.println("cztery"); //to sie pierwsze wyswietlilo...pod warunkiem ze exception zlapie
     }
+128.
+public class MarkList {
+    int num;
+    public static void graceMarks(MarkList obj4){
+        obj4.num += 10;
+    }
+
+    public static void main(String[] args) {
+        MarkList obj1 = new MarkList();  //tylko 1 obiekt jest stworzony. Konstruktory tylko tworza obiekty! inne rzeczy to sciema.
+        MarkList obj2 = obj1;
+        //MarkList obj1 =  null; //to krzyczy bledem poniewaz obj1 jest juz zdefiniowane
+        obj2.num = 60;
+        graceMarks(obj2);
+        System.out.println(obj2.num);  //70 wyswietli
+    }
+}
+
+
  */
